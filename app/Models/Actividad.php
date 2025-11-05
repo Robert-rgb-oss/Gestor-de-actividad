9c4cba51ea7fa1d440e3584edc94494648226bde
Robert <robert@gmail.com>
2025-11-05 17:34:18 +0000

    act

--- app/Models/Actividad.php
+++ app/Models/Actividad.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actividad extends Model
 {
-    //
+    use HasFactory;
+
+    protected$filiable = ['nombre','descripcion','dia','horario'];
+
+    public function inscripciones(){
+        return $this->hasMany(Inscripcion::class);
+    }
+
+    public function alumnos(){
+        return $this->belongsToMany(Alumno::class, 'inscripciones');
+    }
 }
