ad4c05e1a05337b06035f78267915e91a95a4b17
Robert <robert@gmail.com>
2025-11-05 19:38:01 +0000

    act

--- app/Http/Requests/AlumnoUpdateRequest.php
+++ app/Http/Requests/AlumnoUpdateRequest.php
@@ -6,23 +6,19 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AlumnoUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+   
     public function rules(): array
     {
         return [
-            //
+            'nombre' => ['sometimes','required','string','max:120'],
+            'curso' => ['sometimes','required','string','max:60'],
+            'edad' => ['sometimes','required','integer','between:3,99'],
         ];
     }
 }
