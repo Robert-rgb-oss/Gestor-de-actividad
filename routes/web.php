71a5940acddafc7c3f38aad4f0d809e6329dc8ad
Robert <robert@gmail.com>
2025-11-06 19:15:35 +0000

    act

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\InscripcionController;
+use App\Http\Controllers\AlumnoController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});->name('home');
+
+Route::resource('actividades', 'ActividadController::class)->parameters([
+    'actividades' => 'actividad']);
+
+Route::resource('inscripciones', 'InscripcionController::class)->parameters([
+    'inscripciones' => 'inscripcion']);
+
+Route::resource('alumnos', 'AlumnoController::class);
+
+Route::get('/actividades-con-alumnos', [ActividadController::class, 'listadoConAlumnos'])
+->name('actividades.con-alumnos');
+
