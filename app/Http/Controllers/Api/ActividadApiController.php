4f4ff87aee9bd2069bb69e1b4fcf2ba91c62e2a8
Robert <robert@gmail.com>
2025-11-06 19:41:47 +0000

    act

--- app/Http/Controllers/Api/ActividadApiController.php
+++ app/Http/Controllers/Api/ActividadApiController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Actividad;
+use App\Http\Controllers\Controller;
 
 class ActividadApiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $actividades = Actividad::select('id', 'nombre', 'descripcion', 'día', 'hora_inicio', 'hora_fin')
+                                ->orderBy('dia')
+                                ->orderBy('hora_inicio')
+                                ->get();    
+
+        return response()->json([
+            'success' => true,
+            'data' => $actividades,
+            'message' => 'Actividades obtenidas correctamente.',
+            'count' => $actividades->count(),
+        ], 200);
     }
 
     /**
