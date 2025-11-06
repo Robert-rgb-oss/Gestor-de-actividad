e9f9a1586ef32901aec9a5796ac2cd7942034483
Robert <robert@gmail.com>
2025-11-06 16:52:02 +0000

    act

--- app/Http/Requests/ActividadRequest.php
+++ app/Http/Requests/ActividadRequest.php
@@ -16,11 +16,27 @@ class ActividadRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nombre' => ['required','string','max:120'],
-            'descripcion' => ['required','string','max:1000'],
+            'nombre' => ['required','string','min:3','max:120'],
+            'descripcion' => ['required'],
             'dia' => ['required','string','in:Lunes,Martes,Miércoles,Jueves,Viernes'],
             'hora_inicio' => ['required'],
             'hora_fin' => ['required','after:hora_inicio'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'Es nombre obligatorio.',
+            'nombre.string' => 'El nombre debe ser un texto.',
+            'nombre.min' => 'El nombre debe tener minimo 3 caracteres.',
+            'nombre.max' => 'El nombre no debe superar los 120 caracteres.',
+            'descripcion.required' => 'La descripción es obligatoria.',
+            'dia.required' => 'El día es obligatorio.',
+            'dia.in' => 'El día debe ser: lunes, martes, miercoles, jueves o viernes.',
+            'hora_inicio.required' => 'Hora requerida.',
+            'hora_fin.required' => 'Hora fin requerida.',
+            'hora_fin.after' => 'La hora de fin debe ser posterior a la hora de inicio.',
         ];
     }
 }
--- app/Http/Requests/AlumnoRequest.php
+++ app/Http/Requests/AlumnoRequest.php
@@ -18,7 +18,22 @@ class AlumnoRequest extends FormRequest
         return [
             'nombre' => ['required','string','max:120'],
             'curso' => ['required','string','max:60'],
-            'edad' => ['required','integer','between:3,99'],
+            'edad' => ['required','integer','between:3,18'],
+        ];
+    }
+
+    public function message(){
+        return [
+            'nombre.required' => 'El nombre es obligatorio.',
+            'nombre.string' => 'El nombre debe ser un texto.',
+            'nombre.min' => 'El nombre debe tener minimo 3 caracteres.',
+            'nombre.max' => 'El nombre no debe superar los 120 caracteres.',
+            'curso.required' => 'El curso es obligatorio.',
+            'curso.string' => 'El curso debe ser un texto.',
+            'curso.max' => 'El curso no debe superar los 60 caracteres.',
+            'edad.required' => 'La edad es obligatoria.',
+            'edad.integer' => 'La edad debe ser un número entero.',
+            'edad.between' => 'La edad debe estar entre 3 y 18 años.',
         ];
     }
 }
